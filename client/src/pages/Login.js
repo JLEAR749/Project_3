005458f4ead8e67f8abda842172919d292581d70
Abdul Nur <abduljabar@Abduls-MacBook-Pro.local>
2023-04-05 20:21:53 -0500

    added login.js file

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_MATCHUPS } from '../utils/queries';
+
+const Home = () => {
+  
+  return (
+    <div className="card bg-white card-rounded w-50">
+      
+    </div>
+  );
+};
+
+export default Home;
